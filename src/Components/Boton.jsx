5f96c6cf6515e9ddcef0b2b034f1b494b42d9718
franklyn castro <franklyncastro@hotmail.com>
2023-09-13 16:57:29 -0400

    componente boton

---
+++ src/Components/Boton.jsx
@@ -0,0 +1,11 @@
+import '../CSS/Boton.css'
+
+const Boton = () => {
+  return (
+    <div className='container_btn'>
+      <button className='btn'>Crear</button>
+    </div>
+  )
+}
+
+export default Boton
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,14 @@
-import logo from '../../public/img/header.png'
-import '../CSS/header.css'
-
-
+import logo from "../../public/img/header.png";
+import "../CSS/header.css"; 
 
 const Header = () => {
   return (
-    <header className='header'>
-      <img src={logo} alt="img not found" className='logo' />
-    </header>
-  )
-}
+    <div>
+      <header className="header">
+        <img src={logo} alt="img not found" className="logo" />
+      </header>
+    </div>
+  );
+};
 
-export default Header
+export default Header;

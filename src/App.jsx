cdcdfc5c5a9b4022639aaacdad4b21a2de382c4e
franklyncastro <franklyncastro@hotmail.com>
2023-09-14 08:19:32 -0400

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Form from './Components/Form'
 import Header from './Components/Header'
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
      <Header/>
+     <Form/>
     </>
   )
 }

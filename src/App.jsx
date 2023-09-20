125dae77a6fba203712af92617789c27bcd26d14
franklyn castro <franklyncastro@hotmail.com>
2023-09-20 16:15:33 -0400

    mostrar formulario completado

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-
-import './App.css'
-import Header from './Components/Header'
-import Form from './Components/Form'
-import MiOrg from './Components/MiOrg'
-
+import { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Form from "./Components/Form";
+import MiOrg from "./Components/MiOrg";
 
 function App() {
+  const [mostrarForm, setMostrarForm] = useState(false);
 
+  const mostrarFormulario =()=>{
+    setMostrarForm(!mostrarForm)
+  }
 
   return (
     <>
-     <Header/>
-     <Form/>
-     <MiOrg/>
+      <Header />
+      {mostrarForm === false ? <div> </div> : <Form /> }
+      <MiOrg mostrarFormulario ={mostrarFormulario} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/MiOrg.jsx
+++ src/Components/MiOrg.jsx
@@ -1,20 +1,12 @@
-import {useState} from 'react'
 import '../CSS/MiOrg.css'
 
 
-const MiOrg = () => {
-    const [change, setChange] = useState(true)
-
-    const changeOnClick =()=>{
-        setChange(!change)
-    }
-
-
+const MiOrg = ({mostrarFormulario}) => {
+  
   return (
     <section className='orgSection'>
         <h3 className='title'>Mi organización</h3>
-        <img src="/img/add.png" alt="icon add" onClick={changeOnClick}/>
-      
+        <img src="/img/add.png" alt="icon add" onClick={mostrarFormulario}/>
     </section>
   )
 }

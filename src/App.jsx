21252c78316c9ad7273a2295ecff520250436bf5
franklyncastro <franklyncastro@hotmail.com>
2023-10-03 22:54:50 -0400

    crear usuario completado

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,29 @@ import { team } from "./Components/data";
 
 function App() {
   const [mostrarForm, setMostrarForm] = useState(false);
+  const [Users, setUsers] = useState([]);
 
   const mostrarFormulario = () => {
     setMostrarForm(!mostrarForm);
+  };
+
+  const registerUser = (user) => {
+    setUsers([...Users, user]);
   };
 
   return (
     <>
       <Header />
 
-      {mostrarForm === false ? <div> </div> : <Form />}
+      {/* {mostrarForm === false ? <div> </div> : <Form registerUser={registerUser}/>} */}
+
+      {mostrarForm && <Form registerUser={registerUser} />}
 
       <MiOrg mostrarFormulario={mostrarFormulario} />
-      {team.map((team, index) => {
-        return <Equipo key={index} team={team} />;
-      })}
-      
+
+      {team.map((team, index) => (
+        <Equipo key={index} team={team} Users={Users.filter(usr => usr.equipo === team.title )} />
+      ))}
     </>
   );
 }
--- src/Components/Equipo.jsx
+++ src/Components/Equipo.jsx
@@ -3,18 +3,25 @@ import Card from "./Card";
 
 const equipo = ({ team, Users }) => {
 
+
   return (
-    <section
-      className={style.container}
-      style={{ backgroundColor: team.colorTwo }}
-    >
-      <h3 style={{ borderColor: team.colorOne }}>{team.title}</h3>
-      <div className={style.team}>
-        {Users.map((u, i) => (
-          <Card key={i} colorCard={team.colorOne} Users={u}/>
-        ))}
-      </div>
-    </section>
+    <>
+      {
+      (Users.length > 0 && 
+        <section
+          className={style.container}
+          style={{ backgroundColor: team.colorTwo }}
+        >
+          <h3 style={{ borderColor: team.colorOne }}>{team.title}</h3>
+          <div className={style.team}>
+            {Users.map((u, i) => (
+              <Card key={i} colorCard={team.colorOne} Users={u} />
+            ))}
+          </div>
+        </section>
+      )
+      }
+    </>
   );
 };
 
